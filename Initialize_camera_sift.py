15d52a66e8cd1c70c22e86b1c776da88f90c90fa
Mohamed A. Mostafa <mocshamohamed@gmail.com>
2024-04-16 21:53:28 -0400

    direction detection updates

--- Initialize_camera_sift.py
+++ Initialize_camera_sift.py
@@ -12,7 +12,6 @@ class MotionDetector():
     def __init__(self):
         self.frame_counter=0
         self.recent_frames = collections.deque(maxlen=7)
-        self.recent_center_points = collections.deque(maxlen=7)
         self.second_avg_frames = collections.deque(maxlen=10)
         self.recent_difference = collections.deque(maxlen=5)
         self.recent_brightness = collections.deque(maxlen=3) # save last bright_std_mean. difference gives brightness increase/deacrease.
@@ -27,7 +26,8 @@ class MotionDetector():
         image_fg = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         image_fg = cv2.resize(image_fg, (320, 240))
         
-        
+        sift = cv2.SIFT_create()
+        kp, des = sift.detectAndCompute(image_fg, None)
         
         self.recent_frames.append(image_fg)
         avg_frame=np.min(self.recent_frames, axis=0).astype(np.int8)
@@ -35,17 +35,17 @@ class MotionDetector():
             self.second_avg_frames.append(avg_frame)
         # image = np.mean(self.recent_frames, axis=0).astype(np.uint8)
         
-        image = np.abs(image_fg - avg_frame)###
+        image_bg = np.abs(image_fg - avg_frame)###
         
         
         
         #image = np.abs(image - np.mean(self.second_avg_frames, axis=0).astype(np.int16))
         
-        image_bg = cv2.medianBlur(image.astype(np.uint8), 13)
-        # image_bg = image_bg[image_bg > 10]
-        ret, thresh = cv2.threshold(image_bg, np.median(image_bg[image_bg > 10]), 255, cv2.THRESH_BINARY)
+        # image_bg = cv2.medianBlur(image_fg.astype(np.uint8), 13)
+        image_bg = image_bg[image_bg > 10]
+        ret, thresh = cv2.threshold(image_fg, np.median(image_bg[image_bg > 10]), 255, cv2.THRESH_BINARY)
         
-        thresh_cp = thresh.copy()
+        # thresh_cp = thresh.copy()
         
         
         
@@ -54,35 +54,16 @@ class MotionDetector():
         
         
         # contours,hierarchy = cv2.findContours(thresh, 1, 2)
-        contours1, hierarchy1 = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        image = cv2.drawContours(image_bg, contours1, -1, 30, 2, cv2.LINE_AA)
-        if len(contours1) == 0:
-            self.recent_center_points.clear()
-            return image
-        cnt = max(contours1, key = cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(image,(x,y),(x+w,y+h),128,2)
-        center_point = (x+(w//2),y+(h//2))
+        # contours1, hierarchy1 = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # image = cv2.drawContours(image_bg, contours1, -1, 30, 2, cv2.LINE_AA)
+        # if len(contours1) == 0:
+        #     return image
+        # cnt = max(contours1, key = cv2.contourArea)
+        # x,y,w,h = cv2.boundingRect(cnt)
+        # cv2.rectangle(image,(x,y),(x+w,y+h),128,2)
         
-        if center_point == (160, 120):
-            return image
-        print(center_point)
-        cv2.circle(image,center_point,thickness=3,color=255,radius=3)
+        image_fg = cv2.drawKeypoints(thresh, kp, image_fg, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         
-        self.recent_center_points.append(center_point)
-        
-        # print(np.average(self.recent_center_points[0,:].astype(np.int8)))
-        # print(np.mean(self.recent_center_points, axis=0)[0])
-        total = [total := i[0] for i in self.recent_center_points]
-        if len(total) > 7:
-            return image
-        
-        ix = center_point[0] - np.average(total).astype(np.int8)
-        print(ix) #what ix is at the end of the motion is what matters. or we could get the average of ix over the range of movement.
-        # avg_center=np.average(self.recent_center_points[0,:]).astype(np.int8)
-        # print(avg_center)
-        # ix = center_point[0] - avg_center[0]
-
         
         # self.recent_difference.append(abs(image))
         # diff_std =  np.std(image)
@@ -118,13 +99,13 @@ class MotionDetector():
         #     print ("change array size: ",self.change_array.size/(320*240))
         #     self.change_array = np.array([[]])
         
-        return image
+        return image_fg
     
     def image_capture(self):
         ret, frame = cap.read()
         if ret:
             self.frame_time = cap.get(cv2.CAP_PROP_POS_MSEC)
-            # print((time.time() - cap.get(cv2.CAP_PROP_POS_MSEC))/1000)
+            print((time.time() - cap.get(cv2.CAP_PROP_POS_MSEC))/1000)
         
         image = self.image_process(frame)
         return image
