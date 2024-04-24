2ace8edd9b94aceec21e0af9e06dfc7116e963b9
Mohamed A. Mostafa <mocshamohamed@gmail.com>
2024-04-24 03:44:38 -0400

    adding documentation

--- Initialize_camera.py
+++ Initialize_camera.py
@@ -9,37 +9,68 @@ cap = cv2.VideoCapture(0)
 
 class MotionDetector():
 
-    def __init__(self):
+    def __init__(self, width=320, height=240, edge_thresh=0.25, ix_factor=1/8, thesh_cut=10, blur_size=17, n_stored_frames=7):
+        
         self.frame_counter=0
-        self.recent_frames = collections.deque(maxlen=7)
-        self.recent_center_points = collections.deque(maxlen=7)
-        self.recent_difference = collections.deque(maxlen=5)
+        self.rs_width = width
+        self.rs_height = height
+        self.mid_point = (self.rs_width / 2, self.rs_height / 2)
+        MOVEMENT_EDGE_THRESH = edge_thresh
+        self.low_edge = MOVEMENT_EDGE_THRESH * self.rs_width
+        self.high_edge = (1 - MOVEMENT_EDGE_THRESH) * self.rs_width
+        self.ix_thresh = self.rs_width * ix_factor
+        self.thresh_cut = thesh_cut
+        self.blur_size = blur_size
+        self.recent_frames = collections.deque(maxlen=n_stored_frames)
+        
+        # self.recent_center_points = collections.deque(maxlen=7)
         self.ts2dt = lambda timestamp: datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        self.change_array = [] #add the frames relevant to this change. CHANGE THIS TO JUST REGULAR ARRAY.
+        self.change_array = [] #contains the frames relevant to this change.
         self.num = 0
-        self.total = 0
+        self.all_x = 0
         self.ix = 0
 
 
     def background_subtraction(self, frame):
-        self.recent_frames.append(frame)
-        avg_frame=np.min(self.recent_frames, axis=0).astype(np.int8)
-        # if self.frame_counter%30==0:
-        #     self.second_avg_frames.append(avg_frame)
-        # image = np.mean(self.recent_frames, axis=0).astype(np.uint8)
+        """
+        Perform background subtraction on a given frame.
+
+        Parameters:
+            self: The object itself.
+            frame: The input frame for background subtraction.
+
+        Returns:
+            Numpy image array: The result of the background subtraction.
+        """
         
-        return np.abs(frame - avg_frame)###
+        self.recent_frames.append(frame)
+        min_frame=np.min(self.recent_frames, axis=0).astype(np.int8)
+        
+        return np.abs(frame - min_frame)
     
     
     def contour_process(self, frame):
-        ret, thresh = cv2.threshold(frame, np.median(frame[frame > 10]), 255, cv2.THRESH_BINARY)
+        """
+        Process the contours in a given frame.
+
+        Parameters:
+            self: The object itself.
+            frame: The input frame for contour processing.
+
+        Returns:
+            Tuple: A tuple containing the processed frame with contours drawn and the center point of the largest contour.
+                   If no contours are found, the processed frame is returned with a value of None for the center point.
+        """
+        
+        # threshold for values greater than the median above thresh_cut (10), since most of the bs_image is dark and has low values.
+        ret, thresh = cv2.threshold(frame, np.median(frame[frame > self.thresh_cut]), 255, cv2.THRESH_BINARY)
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         image_ct = cv2.drawContours(frame, contours, -1, 30, 2, cv2.LINE_AA)
         if len(contours) == 0:
             return image_ct, None
         
-        cnt = max(contours, key = cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(cnt)
+        cnt = max(contours, key = cv2.contourArea) # find the largest contour
+        x,y,w,h = cv2.boundingRect(cnt) # x,y is top left of the bounding rectangle
         cv2.rectangle(image_ct,(x,y),(x+w,y+h),128,2)
         center_point = (x+(w//2),y+(h//2))
         
@@ -47,47 +78,60 @@ class MotionDetector():
         
         
     def counter_process(self):
+        """
+        A function that processes the movement based on the array of cross-movement center points.
+        """
         # print ("no movement")
-        self.total = [total := i[0] for i in self.change_array]
+        self.all_x = [x_coord := i[0] for i in self.change_array] #pull x coordinates from the change array
         
-        self.ix = np.average(self.total[0:len(self.total)//2]).astype(np.int8) - np.average(self.total[len(self.total)//2:len(self.total)]).astype(np.int8)
-        movement_size = len(self.total)
         
-        # if movement_size > 0:
-        #     print(movement_size)
+        # Difference between average change in x coordinates of the first and second halfs of the array.
+        self.ix = np.average(self.all_x[0:len(self.all_x)//2]).astype(np.int8) - np.average(self.all_x[len(self.all_x)//2:len(self.all_x)]).astype(np.int8)
+        movement_size = len(self.all_x)
+        
         if movement_size > 0:
-            # print (self.total[-1], self.ix)
-            if self.ix > 40 and self.total[-1]>240: #ix change positive + noise, last movement at the end of the frame
+            #print(movement_size) 
+            # print (self.all_x[-1], self.ix)
+            
+            if self.ix > self.ix_thresh and self.all_x[-1]>self.high_edge: #ix change -> positive + noise, last movement at the end of the frame
                 self.num += 1
-                print ("person in /- ", "Direction: ", self.ix, " Counter: ", self.num)
+                print ("IN ", self.ts2dt(time.time()), " Direction: ", self.ix, " Counter: ", self.num)
                 # time.sleep(1)
             
-            elif self.ix < -40 and self.total[-1]<80: #ix change negative - noise, last movement at the beggening of the frame
+            elif self.ix < -self.ix_thresh and self.all_x[-1]<self.low_edge: #ix change -> negative - noise, last movement at the beginning of the frame
                 self.num -= 1
-                print ("person out /- ", "Direction: ", self.ix, " Counter: ", self.num)
+                print ("OUT ", self.ts2dt(time.time()), " Direction: ", self.ix, " Counter: ", self.num)
                 
-            self.recent_center_points.clear()
+            # self.recent_center_points.clear()
             self.change_array.clear()
             
             
     def image_process(self, frame):
-        # if time.time() - (self.frame_time) > 0.05: # if it's taking too much time to process the frame, skip it
-        #     return None
+        """
+        Process the input frame by converting to grayscale, resizing, performing background subtraction, and contour processing.
+        
+        Parameters:
+            self: The object itself.
+            frame: The input frame to be processed.
+        
+        Returns:
+            numpy image array: The processed image after background subtraction, blurring, and contour processing.
+        """
         
         self.frame_counter+=1
         image_fg = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        image_fg = cv2.resize(image_fg, (320, 240))
+        image_fg = cv2.resize(image_fg, (self.rs_width, self.rs_height))
 
         image_bs = self.background_subtraction(image_fg)
-        image_med_b = cv2.medianBlur(image_bs.astype(np.uint8), 13)
+        image_med_b = cv2.medianBlur(image_bs.astype(np.uint8), self.blur_size)
         
         image_bs, center_point = self.contour_process(image_med_b)
         
-        if center_point == None:
+        if center_point == None: #no contours (motion) on the background subtracted image
             return image_med_b
         
         # still need to account one person stops at the middle. so record the end position and compare later movement if starts at the same place. 
-        elif center_point == (160, 120):
+        elif center_point == self.mid_point: #One contour of the whole frame and the center is at the middle of the frame, meaning movement stopped.
             
             self.counter_process()
             return image_med_b
@@ -95,18 +139,22 @@ class MotionDetector():
         # print(center_point)
         cv2.circle(image_med_b,center_point,thickness=3,color=255,radius=3)
         
-        # self.recent_center_points.append(center_point)
         self.change_array.append(center_point)
         
-        # print(np.average(self.recent_center_points[0,:].astype(np.int8)))
-        # print(np.mean(self.recent_center_points, axis=0)[0])
-        if len(self.change_array) < 10:
-            return image_med_b
+        # if len(self.change_array) < 10: #not enough for judgement
+        #     return image_med_b
         
         return image_med_b
     
     
     def image_capture(self):
+        """
+        Captures an image from the camera and processes it.
+        The function then calls the `image_process()` method to process the captured frame and returns the processed image.
+
+        Returns:
+            numpy.ndarray: The processed image.
+        """
         ret, frame = cap.read()
         if ret:
             self.frame_time = cap.get(cv2.CAP_PROP_POS_MSEC)
@@ -117,9 +165,12 @@ class MotionDetector():
         
         
     def image_show(self):
+        """
+        Displays the captured frames from the camera in a window.
+        """
         while True:
-            # Capture frame-by-frame
             
+            # Capture frame-by-frame
             image = self.image_capture()
             
             # Display the resulting frame
@@ -129,8 +180,19 @@ class MotionDetector():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-x = MotionDetector()
-x.image_show()
+
+#Setting the parameters.
+width=320
+height=240
+edge_thresh=0.25
+ix_factor=1/8
+thesh_cut=10
+blur_size=17
+n_stored_frames=7
+
+#Initialize the MotionDetector object
+Motion = MotionDetector(width, height, edge_thresh, ix_factor, thesh_cut, blur_size, n_stored_frames)
+Motion.image_show()
 
 # Release the webcam and close the window
 cap.release()
